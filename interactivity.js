fd00e04d0f85c5e1b8b070268daa8718f3798f44
Triantafyllos Kalaitzidis <kalaitzidistriantafyllos@gmail.com>
2024-03-04 17:16:09 +0200

    Github Pages Setup

---
+++ interactivity.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+// Toggle each section
+    $(".toggle-header").click(
+        function () 
+        {
+        var targetId = $(this).data("target");
+
+        $(".toggle-content").not("#" + targetId).slideUp(300);
+        // Toggle the clicked section
+        $("#" + targetId).slideToggle(300);
+    });
+
+    // Toggle first section on start
+    $(".toggle-header:first").click();
+
+
+    $(".col-lg-6[id$='-o']").mouseenter(function () {
+        var correspondingId = $(this).attr("id").replace("-o", "-i");
+        $("#" + correspondingId).slideDown(100);
+    });
+
+    $(".col-lg-6[id$='-o']").mouseleave(function () {
+        var correspondingId = $(this).attr("id").replace("-o", "-i");
+        $("#" + correspondingId).slideUp(100);
+    });
+});
+
+
